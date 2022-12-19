3f0f4e42cd1910da3ddb49d429252684b48ba19b
Sam Ransom <sjransom@gmail.com>
2022-12-19 19:23:24 +0000

    setup prettier, fix eslint etc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,33 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
+        semi: 0,
+        'eol-last': 2,
+        'no-import-assign': 1,
+        'no-constant-condition': 2,
+        'no-dupe-keys': 2,
+        'no-empty': 2,
+        'no-extra-boolean-cast': 2,
+        'no-prototype-builtins': 2,
+        'no-undef': 2,
+        'no-underscore-dangle': 2,
+        'no-unreachable': 2,
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error'],
         'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
-  ],
-};
+        '@typescript-eslint/no-shadow': 'error',
+        'no-useless-escape': 2,
+        'no-console': 1,
+        'no-var': 2,
+        strict: [2, 'global'],
+        'comma-dangle': ['error', 'never'],
+        'prettier/prettier': 'error'
+      }
+    }
+  ]
+}
