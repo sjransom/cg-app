5a09ec67a2041673a7e93f8f99cb9ed1b4db3e24
Sam Ransom <sjransom@gmail.com>
2023-02-16 15:38:50 +0000

    extend AppButton

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
-import { Button } from 'react-native'
+import { Pressable, StyleSheet, Text } from 'react-native'
+import { COLORS } from '../utils/colors'
+import { SPACING } from '../utils/spacing'
 
 interface AppButtonProps {
+  type: 'PRIMARY' | 'SECONDARY'
   title: string
   onPress(): void
 }
 
-const AppButton: React.FC<AppButtonProps> = ({ title, onPress }) => {
-  return <Button title={title} onPress={onPress} />
+const AppButton: React.FC<AppButtonProps> = ({ type, title, onPress }) => {
+  return (
+    <Pressable
+      style={[
+        { backgroundColor: type === 'PRIMARY' ? COLORS.black : COLORS.white },
+        styles.button
+      ]}
+      onPress={onPress}>
+      <Text
+        style={[
+          { color: type === 'PRIMARY' ? COLORS.white : COLORS.black },
+          styles.text
+        ]}>
+        {title}
+      </Text>
+    </Pressable>
+  )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: SPACING.s16,
+    paddingVertical: SPACING.s12,
+    paddingHorizontal: SPACING.s32,
+    borderRadius: SPACING.s16
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold'
+  }
+})
 
 export default AppButton
