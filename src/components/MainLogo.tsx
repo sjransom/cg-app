216ff2d06ee37058f6cca9b2b63e5a425550c506
Sam Ransom <sjransom@gmail.com>
2023-02-16 11:22:29 +0000

    MainLogo component

---
+++ src/components/MainLogo.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Image } from 'react-native'
+
+const MainLogo = () => {
+  return <Image source={require('../../assets/images/cg.jpg')} />
+}
+
+export default MainLogo
