c67f4e8d7ec540f07c14068f70348a9830b0e11a
Sam Ransom <sjransom@gmail.com>
2023-02-16 15:07:29 +0000

    MainLogo style

--- src/components/MainLogo.tsx
+++ src/components/MainLogo.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-import { Image } from 'react-native'
+import { Image, StyleSheet, View } from 'react-native'
 
 const MainLogo = () => {
-  return <Image source={require('../../assets/images/cg.jpg')} />
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/cg.jpg')}
+        style={styles.image}
+      />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: 250,
+    height: 250
+  }
+})
 
 export default MainLogo
