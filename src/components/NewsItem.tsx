eb6d7992b289127b7ec5cbd7066788556bd443fd
Sam Ransom <sjransom@gmail.com>
2023-02-17 06:41:02 +0000

    news styling

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import { Post } from '../types'
+import { COLORS } from '../utils/colors'
+import { SPACING } from '../utils/spacing'
 
 interface NewsItemProps {
   post: Post
@@ -8,11 +10,67 @@ interface NewsItemProps {
 
 const NewsItem: React.FC<NewsItemProps> = ({ post }) => {
   return (
-    <View>
-      <Text>{post.heading}</Text>
-      <Text>{post.body}</Text>
+    <View style={styles.card}>
+      <View style={styles.header}>
+        <Image source={{ uri: post.avatar }} style={styles.avatar} />
+        <View>
+          <Text style={styles.name}>{post.name}</Text>
+          <Text style={styles.date}>{post.createdDate}</Text>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.heading}>{post.heading}</Text>
+        <Image source={{ uri: post.image }} style={styles.image} />
+        <Text style={styles.bodyText}>{post.body}</Text>
+        {post.motm && (
+          <Text>
+            Man of the match: <Text style={styles.motm}>{post.motm}</Text>
+          </Text>
+        )}
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: SPACING.s32,
+    padding: SPACING.s12,
+    backgroundColor: COLORS.white
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    marginRight: SPACING.s12
+  },
+  name: {
+    fontSize: 14
+  },
+  date: {
+    fontSize: 10
+  },
+  body: {
+    marginTop: SPACING.s12
+  },
+  heading: {
+    marginBottom: SPACING.s12,
+    fontWeight: 'bold'
+  },
+  image: {
+    height: 200,
+    flex: 1
+  },
+  bodyText: {
+    marginTop: SPACING.s12,
+    marginBottom: SPACING.s12
+  },
+  motm: {
+    fontWeight: 'bold'
+  }
+})
 
 export default NewsItem
--- src/screens/News.tsx
+++ src/screens/News.tsx
@@ -4,22 +4,31 @@ import {
   ListRenderItem,
   SafeAreaView,
   StyleSheet,
+  Text,
   View
 } from 'react-native'
 import Config from 'react-native-config'
 import { fetchData } from '../api/fetchData'
+import AppHeader from '../components/AppHeader'
+import ErrorMessage from '../components/ErrorMessage'
 import NewsItem from '../components/NewsItem'
+import { useAuth } from '../hooks/useAuth'
 import { Post, Posts } from '../types'
+import { SPACING } from '../utils/spacing'
 
 const News = () => {
+  const { loading } = useAuth()
   const [data, setData] = useState<Post[] | null>(null)
+  const [error, setError] = useState<boolean>(false)
 
   const getData = async () => {
+    setError(false)
     try {
       const fetchedData: Posts = await fetchData(`${Config.API_URL}/newsfeed`)
       setData(fetchedData.posts)
     } catch (e) {
       console.log(e, 'unable to fetch data')
+      setError(true)
     }
   }
 
@@ -32,23 +41,27 @@ const News = () => {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        {data && (
+    <SafeAreaView>
+      <View style={styles.container}>
+        {data && !error && (
           <FlatList
             data={data}
             renderItem={renderItem}
             keyExtractor={post => post.id}
+            ListHeaderComponent={<AppHeader text="Latest News" />}
+            showsVerticalScrollIndicator={false}
           />
         )}
+        {error && <ErrorMessage text="Newsfeed not avaible at this time" />}
       </View>
+      {loading && <Text>Loading</Text>}
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    padding: SPACING.s16
   }
 })
 
